457adf9e29740ce92b2207c1d58d28ed62adff4b
Jhon Costa <jhon.costa.dev@gmail.com>
2023-08-23 21:32:52 -0300

    error finished
    Error src null

--- main1.js
+++ main1.js
@@ -8,10 +8,6 @@ let areaFinanceiro = document.querySelector(".area-financeiro")
 let areaConfiguracao = document.querySelector(".area-configuracao")
 let sair = document.querySelector(".sair")
 let btnPublicar = document.querySelector(".publicar")
-let quill = new Quill('#editor', {
-    theme: 'snow',
-    placeholder: 'Digite aqui o conteudo da postagem'
-});
 let menu = [...document.querySelectorAll(".menu")]
 
 menu.map((selecionado)=>{
@@ -43,14 +39,36 @@ menu.map((selecionado)=>{
 
 let criarCard = ()=>{
     let p = document.createElement("p")
+    let p2 = document.createElement("p")
     let div = document.createElement("div")
+    let divImg = document.createElement("div")
+    let img = document.createElement("img")
     let h4 = document.createElement("h4")
     let cTitulo = document.querySelector(".ctitulo").value
+    let cConteudo = document.querySelector("#conteudo").value
     h4.innerHTML = cTitulo
     let cImagem = document.querySelector(".cimagem").value
     let lugarNoticia = document.querySelector(".lugar-noticias")
-    let cConteudo = quill.root.innerHTML
+    let imgF = document.querySelector(".img-noticia")
+   
     let dataAtual = new Date()
+
+    let addIMG = ()=>{
+         let imagemC = document.querySelector("#cptImg")
+         let arquivo = imagemC.files[0];
+
+         if(arquivo){
+            let leitor = new FileReader()
+            leitor.onload = function(e) {
+                img.src = e.target.result;
+        };
+
+            leitor.readAsDataURL(arquivo);
+        }
+
+ 
+    }
+
     let opcoesDeFormatacao = {
     year: 'numeric',
     month: '2-digit',
@@ -61,29 +79,31 @@ let criarCard = ()=>{
     let formatadorDeData = new Intl.DateTimeFormat('pt-BR', opcoesDeFormatacao);
     let dataHoraFormatada = formatadorDeData.format(dataAtual);
     p.classList.add('p-data')
+
     p.innerHTML = "Publicado em " + dataHoraFormatada
+    img.classList.add("img-noticia")
+    addIMG()
     div.classList.add("noticia")
+    p2.innerHTML = cConteudo
     
 
+    divImg.appendChild(img)
     div.appendChild(h4)
     div.appendChild(p)
+    div.appendChild(divImg)
+    div.appendChild(p2)
 
     lugarNoticia.appendChild(div)
 
 
 }
 
-let limparAreaPostagem = ()=>{
-    let cTitulo = ""
-    let cImagem = ""
-    let cConteudo = ""
-}
+
 
 btnPublicar.addEventListener("click",(e)=>{
     e.preventDefault()
 
     criarCard()
-    limparAreaPostagem()
 })
 
 
